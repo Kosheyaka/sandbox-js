e6280591b73fccaa7e5e30fad108c00dc8750b46
Dmitry Volkov <d.volkov@bothelp.io>
2022-01-25 09:11:34 +0300

    unit-tests updated

--- src/helpers/array.spec.ts
+++ src/helpers/array.spec.ts
@@ -59,60 +59,274 @@ describe('Helpers: Array', () => {
     });
   });
 
-  it('flatten', () => {
-    const result = flatten([
-      [
-        [1, 2, 3],
-        [3, 4, 5],
-      ],
-      [12, 13],
-    ]);
-    const expected = [1, 2, 3, 3, 4, 5, 12, 13];
+  describe('flatten', () => {
+    it('Should make flat array from array of 2 levels', () => {
+      const result = flatten([
+        [
+          [1, 2, 3],
+          [4, 5, 6],
+        ],
+        [7, 8],
+      ]);
+      const expected = [1, 2, 3, 4, 5, 6, 7, 8];
 
-    expect(result).toEqual(expected);
+      expect(result).toEqual(expected);
+    });
+    it('Should make flat array from array of 2+ levels', () => {
+      const result = flatten([
+        [
+          [1, 2],
+          [3, [4, 5]],
+        ],
+        [12, 13],
+      ]);
+      const expected = [1, 2, 3, 4, 5, 12, 13];
+
+      expect(result).toEqual(expected);
+    });
+    it('Should not mutate input', () => {
+      const input = [
+        [
+          [1, 2],
+          [3, [4, 5]],
+        ],
+        [12, 13],
+      ];
+      const clone = [...input];
+      flatten([
+        [
+          [1, 2],
+          [3, [4, 5]],
+        ],
+        [12, 13],
+      ]);
+
+      expect(input).toEqual(clone);
+    });
   });
 
-  it('moveItemToStart', () => {
+  describe('moveItemToStart', () => {
     interface Item {
       isTarget: boolean;
       id: number;
     }
-    const initialArray: Item[] = [
-      { id: 1, isTarget: false },
-      { id: 2, isTarget: false },
-      { id: 3, isTarget: true },
-      { id: 4, isTarget: false },
-    ];
-    const initialArrayClone = [...initialArray];
 
-    const result = moveItemToStart<Item>(
-      initialArray,
-      ({ isTarget }) => isTarget,
-    );
+    it('Item found and moved to index 0, input not mutated', () => {
+      const initialArray: Item[] = [
+        { id: 0, isTarget: false },
+        { id: 1, isTarget: false },
+        { id: 2, isTarget: false },
+        { id: 3, isTarget: true },
+        { id: 4, isTarget: false },
+        { id: 5, isTarget: false },
+      ];
+      const initialArrayClone = [...initialArray];
 
-    expect(initialArray).toEqual(initialArrayClone); // Must not mutate
-    expect(result[0]).toEqual({ id: 3, isTarget: true });
+      const result = moveItemToStart<Item>(
+        initialArray,
+        ({ isTarget }) => isTarget,
+      );
+
+      expect(initialArray).toEqual(initialArrayClone);
+      expect(result[0]).toEqual({ id: 3, isTarget: true });
+    });
+    it('Item not found, result equals original, input not mutated', () => {
+      const initialArray: Item[] = [
+        { id: 5, isTarget: false },
+        { id: 4, isTarget: false },
+        { id: 3, isTarget: false },
+        { id: 2, isTarget: false },
+        { id: 1, isTarget: false },
+        { id: 0, isTarget: false },
+      ];
+      const initialArrayClone = [...initialArray];
+
+      const result = moveItemToStart<Item>(
+        initialArray,
+        ({ isTarget }) => isTarget,
+      );
+
+      expect(initialArray).toEqual(initialArrayClone);
+      expect(result[0]).toEqual({ id: 5, isTarget: false });
+    });
+    it('Few item found, first of them moved to start, input not mutated', () => {
+      const initialArray: Item[] = [
+        { id: 0, isTarget: false },
+        { id: 1, isTarget: true },
+        { id: 2, isTarget: true },
+        { id: 3, isTarget: false },
+        { id: 4, isTarget: false },
+        { id: 5, isTarget: false },
+      ];
+      const initialArrayClone = [...initialArray];
+
+      const result = moveItemToStart<Item>(
+        initialArray,
+        ({ isTarget }) => isTarget,
+        true,
+      );
+
+      expect(initialArray).toEqual(initialArrayClone);
+      expect(result[0]).toEqual({ id: 1, isTarget: true });
+      expect(result[2]).toEqual({ id: 2, isTarget: true });
+    });
+
+    describe('Item found at start and end', () => {
+      it('searchFromStart = true, means last item should stay', () => {
+        const initialArray: Item[] = [
+          { id: 0, isTarget: true },
+          { id: 1, isTarget: false },
+          { id: 2, isTarget: false },
+          { id: 3, isTarget: false },
+          { id: 4, isTarget: false },
+          { id: 5, isTarget: true },
+        ];
+        const initialArrayClone = [...initialArray];
+
+        const result = moveItemToStart<Item>(
+          initialArray,
+          ({ isTarget }) => isTarget,
+          true,
+        );
+
+        expect(initialArray).toEqual(initialArrayClone);
+        expect(result[0]).toEqual({ id: 0, isTarget: true });
+        expect(result[result.length - 1]).toEqual({ id: 5, isTarget: true });
+      });
+      it('searchFromStart = false, means last item should move', () => {
+        const initialArray: Item[] = [
+          { id: 0, isTarget: true },
+          { id: 1, isTarget: false },
+          { id: 2, isTarget: false },
+          { id: 3, isTarget: false },
+          { id: 4, isTarget: false },
+          { id: 5, isTarget: true },
+        ];
+        const initialArrayClone = [...initialArray];
+
+        const result = moveItemToStart<Item>(
+          initialArray,
+          ({ isTarget }) => isTarget,
+          false,
+        );
+
+        expect(initialArray).toEqual(initialArrayClone);
+        expect(result[0]).toEqual({ id: 5, isTarget: true });
+        expect(result[1]).toEqual({ id: 0, isTarget: true });
+      });
+    });
   });
 
-  it('moveItemToEnd', () => {
+  describe('moveItemToEnd', () => {
     interface Item {
       isTarget: boolean;
       id: number;
     }
-    const initialArray: Item[] = [
-      { id: 1, isTarget: false },
-      { id: 2, isTarget: false },
-      { id: 3, isTarget: true },
-      { id: 4, isTarget: false },
-    ];
-    const initialArrayClone = [...initialArray];
 
-    const result = moveItemToEnd<Item>(
-      initialArray,
-      ({ isTarget }) => isTarget,
-    );
+    it('Item found and moved to index 0, input not mutated', () => {
+      const initialArray: Item[] = [
+        { id: 0, isTarget: false },
+        { id: 1, isTarget: false },
+        { id: 2, isTarget: false },
+        { id: 3, isTarget: true },
+        { id: 4, isTarget: false },
+        { id: 5, isTarget: false },
+      ];
+      const initialArrayClone = [...initialArray];
 
-    expect(initialArray).toEqual(initialArrayClone); // Must not mutate
-    expect(result[result.length - 1]).toEqual({ id: 3, isTarget: true });
+      const result = moveItemToEnd<Item>(
+        initialArray,
+        ({ isTarget }) => isTarget,
+      );
+
+      expect(initialArray).toEqual(initialArrayClone);
+      expect(result[result.length - 1]).toEqual({ id: 3, isTarget: true });
+    });
+    it('Item not found, result equals original, input not mutated', () => {
+      const initialArray: Item[] = [
+        { id: 5, isTarget: false },
+        { id: 4, isTarget: false },
+        { id: 3, isTarget: false },
+        { id: 2, isTarget: false },
+        { id: 1, isTarget: false },
+        { id: 0, isTarget: false },
+      ];
+      const initialArrayClone = [...initialArray];
+
+      const result = moveItemToEnd<Item>(
+        initialArray,
+        ({ isTarget }) => isTarget,
+      );
+
+      expect(initialArray).toEqual(initialArrayClone);
+      expect(result[0]).toEqual({ id: 5, isTarget: false });
+    });
+    it('Few item found, first of them moved to start, input not mutated', () => {
+      const initialArray: Item[] = [
+        { id: 0, isTarget: false },
+        { id: 1, isTarget: true },
+        { id: 2, isTarget: true },
+        { id: 3, isTarget: false },
+        { id: 4, isTarget: false },
+        { id: 5, isTarget: false },
+      ];
+      const initialArrayClone = [...initialArray];
+
+      const result = moveItemToEnd<Item>(
+        initialArray,
+        ({ isTarget }) => isTarget,
+        true,
+      );
+
+      expect(initialArray).toEqual(initialArrayClone);
+      expect(result[result.length - 1]).toEqual({ id: 1, isTarget: true });
+      expect(result[1]).toEqual({ id: 2, isTarget: true });
+    });
+
+    describe('Item found at start and end', () => {
+      it('searchFromStart = true, means last item should move', () => {
+        const initialArray: Item[] = [
+          { id: 0, isTarget: true },
+          { id: 1, isTarget: false },
+          { id: 2, isTarget: false },
+          { id: 3, isTarget: false },
+          { id: 4, isTarget: false },
+          { id: 5, isTarget: true },
+        ];
+        const initialArrayClone = [...initialArray];
+
+        const result = moveItemToEnd<Item>(
+          initialArray,
+          ({ isTarget }) => isTarget,
+          true,
+        );
+
+        expect(initialArray).toEqual(initialArrayClone);
+        expect(result[0]).toEqual({ id: 1, isTarget: false });
+        expect(result[result.length - 1]).toEqual({ id: 0, isTarget: true });
+        expect(result[result.length - 2]).toEqual({ id: 5, isTarget: true });
+      });
+      it('searchFromStart = false, means last item should stay', () => {
+        const initialArray: Item[] = [
+          { id: 0, isTarget: true },
+          { id: 1, isTarget: false },
+          { id: 2, isTarget: false },
+          { id: 3, isTarget: false },
+          { id: 4, isTarget: false },
+          { id: 5, isTarget: true },
+        ];
+        const initialArrayClone = [...initialArray];
+
+        const result = moveItemToEnd<Item>(
+          initialArray,
+          ({ isTarget }) => isTarget,
+          false,
+        );
+
+        expect(initialArray).toEqual(initialArrayClone);
+        expect(result[0]).toEqual({ id: 0, isTarget: true });
+        expect(result[result.length - 1]).toEqual({ id: 5, isTarget: true });
+      });
+    });
   });
 });
